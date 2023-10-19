3456123ba94bcb488dc2ddbe9add9c0cb4e53f69
9EED <88392191+9EED@users.noreply.github.com>
2023-10-19 02:57:55 +0100

    *

--- functions.js
+++ functions.js
@@ -1,52 +1,40 @@
 all_digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-class number{
-    constructor(digits, base){
-        this.error = false
 
-        if(typeof(digits) != "string" || typeof(base) != "number") this.error = true
-        this.digits = digits.split("").map((a)=>{
-            if(all_digits.search(a.toUpperCase()) == -1) this.error = true
-            return all_digits.search(a.toUpperCase())
-        }).reverse();
-        for(let i in this.digits){
-            if(this.digits[i] >= base){
-                this.digits = [0]
-                this.error = true
-                break
-            }
-        }
-
-        if(base < 2) this.error = true
-        this.base = base;
-    }
-    get_string(){
-        if(this.digits.length == 0) return "0"
-        let string = this.digits.map((a)=>{return all_digits[a]}).reverse().join("")
-        return string
-    }
-    get_decimal_int(){
-        let decimal = 0
-        for(let i in this.digits){
-            decimal += this.digits[i] * this.base ** i
-        }
-        return decimal
-    }
-    convert_base(new_base){
-        let old_div = this.get_decimal_int()
-        let new_digits = []
-        let i = 0
-        while(old_div > 0 && i < 64){
-            i++
-            let remainder = old_div % new_base
-            let div = (old_div - remainder) / new_base
-            old_div = div
-            new_digits.push(remainder)
-        }
-
-        this.base = new_base
-        this.digits = new_digits
-        return this
-    }
+function int_to_digits(int){
+    let text = ''+int
+    return text.split("").map((a)=>{
+        return all_digits.search(a.toUpperCase())
+    }).reverse()
 }
-
+function text_to_digits(text){
+    return text.split("").map((a)=>{
+        return all_digits.search(a.toUpperCase())
+    }).reverse()
+}
+function digits_to_text(digits){
+    if(digits.length == 0) return "0"
+    let text = digits.map((a)=>{return all_digits[a]}).join("")
+    return text
+}
+function to_decimal(digits, base){
+    if(typeof(digits) != 'object') return 0
+    let decimal = 0
+    for(let i in digits){
+        decimal += digits[i] * base ** i
+    }
+    return decimal
+}
+function base_convert(digits, base1, base2){
+    let old_div = to_decimal(digits, base1)
+    let new_digits = []
+    let i = 0
+    while(old_div > 0 && i < 64){
+        i++
+        let remainder = old_div % base2
+        let div = (old_div - remainder) / base2
+        old_div = div
+        new_digits.push(remainder)
+    }
+    return new_digits.reverse()
+}
--- script.js
+++ script.js
@@ -3,3 +3,70 @@ setInterval(()=>{
     if(clicky.innerHTML == "_") clicky.innerHTML = " "
     else clicky.innerHTML = "_"
 },600)
+
+let html_container = document.getElementById('container')
+let global_int = 0
+let bases = [
+    {
+        name: 'binary',
+        int: 2,
+        get_string: () => {
+            return digits_to_text(base_convert(int_to_digits(global_int), 10, 2))
+        }
+    },
+    {
+        name: 'octal',
+        int: 8,
+        get_string: () => {
+            return digits_to_text(base_convert(int_to_digits(global_int), 10, 8))
+        }
+    },
+    {
+        name: 'decimal',
+        int: 10,
+        get_string: () => {
+            return digits_to_text(base_convert(int_to_digits(global_int), 10, 10))
+        }
+    },
+    {
+        name: 'hexadecimal',
+        int: 16,
+        get_string: () => {
+            return digits_to_text(base_convert(int_to_digits(global_int), 10, 16))
+        }
+    },
+    /*{
+        name: 'gray_code',
+        int: 0,
+        get_string: () => {
+            return 0
+        }
+    }*/
+]
+
+html_container.innerHTML = ''
+for(let base of bases){
+    let child_html = `
+        <div class="slot" id="${base.name}_div">
+            <div>${base.name}${base.int != 0 ? ' - base_'+base.int : ''}:</div>
+            <input type="text" value="${base.get_string()}" id="${base.name}_input" data-int="${base.int}"></input>
+        </div>
+    `
+    html_container.innerHTML += child_html
+}
+
+function update_html(skip){
+    for(let base of bases){
+        document.getElementById(base.name+'_input').value = base.get_string()
+
+        if(base.int != 0) document.getElementById(base.name+'_input').addEventListener('input', (event)=>{
+            let input = event.target.value
+            let b = parseInt(event.target.getAttribute('data-int'))
+            global_int = to_decimal(text_to_digits(input), b)
+
+            update_html(event.target)
+        })
+    }
+}
+
+update_html(0)
